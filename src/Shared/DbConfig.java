c214efa45b833a75129b3565af3a9eb1463d9e76
khalifa <mednaceur199530@gmail.com>
2022-06-01 15:41:20 +0100

    config Envirement

---
+++ src/Shared/DbConfig.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Shared;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DbConfig {
+        public static String URL = "jdbc:mysql://localhost:3306/ehire"; 
+    public static String UserName = "root"; 
+    public static String Password = ""; 
+}
