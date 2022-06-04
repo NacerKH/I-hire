2cd1befd95caa1b5c97e1af96e36f4f69534789c
Dhia Snoussi <69083299+DhiaSn@users.noreply.github.com>
2022-06-04 16:17:15 +0100

    add empty class

--- src/GUI/empty.java
+++ src/GUI/empty.java
@@ -3,22 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Main;
+package GUI;
 
 /**
  *
  * @author Dhiaeddsn
  */
-public class EHireSolution {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-        /*
-         * To DO: test here your crud
-         */
-    }
+public class empty {
     
 }
